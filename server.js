176ae88948bc95c6b456cfd8f4a18707465d5dae
larmong <larmong@naver.com>
2023-07-25 00:58:04 +0900

    [FIX] cors 설정

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-import {ApolloServer, gql} from "apollo-server";
-import fetch from "node-fetch"
+import { ApolloServer, gql } from "apollo-server";
+import cors from "cors";
+import fetch from "node-fetch";
 
 const typeDefs = gql`
     # Query
@@ -15,29 +16,48 @@ const typeDefs = gql`
         stationLongitude: String!
         stationId: String!
     }
-`
+`;
 
 const resolvers = {
     Query: {
-        allBikeList(){
+        allBikeList() {
             return fetch("http://openapi.seoul.go.kr:8088/77447a58706c617237364d6a694774/json/bikeList/1/1000/")
               .then((res) => res.json())
               .then((json) => json.rentBikeStatus.row);
         },
     },
+};
 
-}
+const app = express();
+
+// CORS 설정
+const allowedOrigins = [
+    "https://bike-finder-larmong.netlify.app",
+    "http://localhost:3000",
+];
+app.use(
+  cors({
+      origin: function (origin, callback) {
+          if (!origin || allowedOrigins.includes(origin)) {
+              callback(null, true);
+          } else {
+              callback(new Error("Not allowed by CORS"));
+          }
+      },
+      credentials: true,
+  })
+);
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     introspection: true,
     playground: true,
-    cors: {
-        origin: "https://bike-finder-app-56767ec70f0a.herokuapp.com",
-    }
 });
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
+server.applyMiddleware({ app });
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`);
 });
