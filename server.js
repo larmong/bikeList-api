a2c36ba8305121c5c20c8a7f4531b5938180b924
larmong <larmong@naver.com>
2023-07-25 00:04:48 +0900

    test

--- server.js
+++ server.js
@@ -1,43 +1,45 @@
-import { ApolloServer, gql } from "apollo-server";
-import fetch from "node-fetch";
+import {ApolloServer, gql} from "apollo-server";
+import fetch from "node-fetch"
+
 
 const typeDefs = gql`
     # Query
+#    type Query {
+#        allBikeList: [bikeList!]!
+#    }
+#    type bikeList {
+#        rackTotCnt: String!
+#        stationName: String!
+#        parkingBikeTotCnt: String!
+#        shared: String!
+#        stationLatitude: String!
+#        stationLongitude: String!
+#        stationId: String!
+#    }
     type Query {
-        allBikeList: [bikeList!]!
+        message: String!
+        url: String!
     }
-    type bikeList {
-        rackTotCnt: String!
-        stationName: String!
-        parkingBikeTotCnt: String!
-        shared: String!
-        stationLatitude: String!
-        stationLongitude: String!
-        stationId: String!
-    }
-`;
+`
+
+// const resolvers = {
+//     Query: {
+//         allBikeList(){
+//             return fetch("http://openapi.seoul.go.kr:8088/77447a58706c617237364d6a694774/json/bikeList/1/1000/")
+//               .then((res) => res.json())
+//               .then((json) => json.rentBikeStatus.row);
+//         },
+//     },
+//
+// }
 
 const resolvers = {
     Query: {
-        async allBikeList() {
-            try {
-                const apiUrl = "http://openapi.seoul.go.kr:8088/77447a58706c617237364d6a694774/json/bikeList/1/10/";
-                const proxyOptions = {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                };
-                const response = await fetch(apiUrl, proxyOptions);
-                const json = await response.json();
-                return json.rentBikeStatus.row;
-            } catch (error) {
-                console.error("프록시 요청 오류:", error);
-                throw new Error("서버에서 데이터를 가져오는 중에 오류가 발생했습니다.");
-            }
+        random(){
+            return fetch("https://random-d.uk/api/random")
         },
     },
-};
+}
 
 const server = new ApolloServer({
     typeDefs,
@@ -46,7 +48,7 @@ const server = new ApolloServer({
     playground: true,
     cors: {
         origin: "https://bike-finder-app-56767ec70f0a.herokuapp.com",
-    },
+    }
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
